488b2572785f7e0f5eb3b61f48ef859f5988cb47
Justin Choi <justinchoi6769@gmail.com>
2025-08-24 11:04:14 -0500

    Tidying up files

--- sabersem_plot.py
+++ sabersem_plot.py
@@ -18,11 +18,13 @@ righty_ff_3_2 =  (
 alpha_x, loc_x, scale_x = gamma.fit(righty_ff_3_2['plate_x_std'], floc=0) 
 
 x = np.linspace(righty_ff_3_2['plate_x_std'].min(), righty_ff_3_2['plate_x_std'].max(), 500)
+
 fig, ax = plt.subplots(figsize=(8,8))
 sns.kdeplot(righty_ff_3_2['plate_x_std'], label='KDE of Standard Deviations') 
 ax.plot(x, gamma.pdf(x, a=alpha_x, loc=loc_x, scale=scale_x), label='Best-fitted Gamma Dist.') 
 
-ax.set_xlabel("X-Location Standard Deviation")
-ax.legend() 
-plt.title("3-2 Count Four-Seam Fastballs From RHP")
+ax.set_xlabel("X-Location Standard Deviation", fontsize=13)
+ax.set_ylabel("Density", fontsize=13)
+ax.legend(fontsize=13) 
+plt.title("3-2 Count Four-Seam Fastballs From RHP", fontsize=15)
 plt.show() 
